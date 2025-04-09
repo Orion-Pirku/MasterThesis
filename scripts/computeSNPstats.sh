d2ddd2db77cca65e719d24a5f5554405a6897e9f
Orion Pirku <orionpirku@gmail.com>
2025-04-09 13:41:17 +0200

    wrote script to compute Nucleotide diversity, SNP density and tajimas d in windows of 10kb, 100kb and 1 MB

---
+++ scripts/computeSNPstats.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#BATCH --job-name=compute_snp_stats
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --mem=32G
+#SBATCH --error=:compute_snp_stats.err
+#SBATCH --output=compute_snp_stats.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=pirku@evolbio.mpg.de
+#SBATCH --partition=standard
+
+module load gcc/6.2.1
+module load python/python-miniconda3
+
+cd $HOME/Masters_Thesis
+mkdir -p vcf_stats
+
+for file in vcf/sorted_*; do
+	for i in 10000 100000 1000000; do
+		vcftools --gzvcf "$file" --SNPdensity "${i}" --out "vcf_stats/snp_density_${i}_${file##*/}"
+		vcftools --gzvcf "$file" --window-pi "${i}" --out "vcf_stats/window_pi_${i}_${file##*/}"
+		vcftools --gzvcf "$file" --TajimaD "${i}" --out "vcf_stats/tajimaD_${i}_${file##*/}"
+	done
+done
+echo "done"

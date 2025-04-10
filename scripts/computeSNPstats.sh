fbb9c4c12f7811a7a9137949a9928f1b35ada992
Orion Pirku <orionpirku@gmail.com>
2025-04-10 11:29:13 +0200

    added output_name=.rvmrc and increased time to 2 hours for BATCH run

--- scripts/computeSNPstats.sh
+++ scripts/computeSNPstats.sh
@@ -2,7 +2,7 @@
 #BATCH --job-name=compute_snp_stats
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --mem=32G
 #SBATCH --error=compute_snp_stats.err
 #SBATCH --output=compute_snp_stats.out
@@ -17,10 +17,11 @@ cd $HOME/Masters_Thesis
 mkdir -p vcf_stats
 
 for file in vcf/sorted_*; do
+	output_name=$(basename "$file" .vcf.gz)
 	for i in 10000 100000 1000000; do
-		vcftools --gzvcf "$file" --SNPdensity "${i}" --out "vcf_stats/snp_density_${i}_${file##*/}"
-		vcftools --gzvcf "$file" --window-pi "${i}" --out "vcf_stats/window_pi_${i}_${file##*/}"
-		vcftools --gzvcf "$file" --TajimaD "${i}" --out "vcf_stats/tajimaD_${i}_${file##*/}"
+		vcftools --gzvcf "$file" --SNPdensity "${i}" --out "vcf_stats/snp_density_${i}_${output_name}"
+		vcftools --gzvcf "$file" --window-pi "${i}" --out "vcf_stats/window_pi_${i}_${output_name}"
+		vcftools --gzvcf "$file" --TajimaD "${i}" --out "vcf_stats/tajimaD_${i}_${output_name}"
 	done
 done
 echo "done"

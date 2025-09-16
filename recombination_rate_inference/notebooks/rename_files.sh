af03d0afd864c54a10028e8d10a9942c363084d4
Orion-Pirku <orionpirku@gmail.com>
2025-09-16 11:42:58 +0200

    changed the peak calling algorithm for recombination rate

---
+++ recombination_rate_inference/notebooks/rename_files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+for FILE in "../output/blackcap_recombination_maps/*"; do
+    
---
+++ recombination_rate_inference/scripts/main.py
@@ -0,0 +1,13 @@
+from recombination_rate_inference.src.transform import *
+from recombination_rate_inference.src.plotting import *
+import recombination_rate_inference.src.io as io
+from recombination_rate_inference.src.summarizing import *
+from pybedtools import BedTool
+from typing import cast
+from pathlib import Path
+
+# def compute_jaccard_index()
+
+if __name__=="__main__":
+    pass
+    
